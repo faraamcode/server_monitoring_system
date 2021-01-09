c86630aac575a955b737b6b0337e8674312891b7
Yusuf Adeyemo <yusadolat@gmail.com>
2021-01-09 16:19:16 +0000

    Feat: I added some comment, check app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const http = require('http')
+
+
+const http = require('http') //You dont this anymore, It already in the express package
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //You don't need this too. It already in th express
 const dataRoute = require('./route/data')
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
