91fd986832bf754c7ff093e07d7c44d3a3a1cfae
Yusuf Adeyemo <yusadolat@gmail.com>
2021-01-19 00:32:03 +0000

    Bedy parser comment :zap:

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const bodyParser = require('body-parser'); //You don't need this too. It already in th express
+const bodyParser = require('body-parser'); // Check below comment 
 // whenever I try to remove bodyParser my post will stop working
 const dataRoute = require('./route/data')
 const userRoute = require('./route/user')
 const app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.json())//Comment this and do as the Multiline comment below.
+
+/* 
+app.use(express.json()) // This will worj the same way body-parser works
+*/
 app.use(dataRoute)
 app.use('/user',userRoute)
 app.listen(3000)
